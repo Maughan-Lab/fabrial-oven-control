a9e6db193985b9c4918c862dbff1df741ad234bd
Drew <drewlwhitney@gmail.com>
2025-08-20 17:38:48 -0600

    In the middle of some changes

---
+++ fabrial_oven_control/items/set_setpoint/set_setpoint_widget.py
@@ -0,0 +1,8 @@
+from fabrial import ItemWidget
+
+
+class SetSetpointWidget(ItemWidget):
+    """Set the oven's setpoint and stabilize; widget."""
+
+    def __init__(self, port: str):
+        pass
